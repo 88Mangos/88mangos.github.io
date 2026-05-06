292754a8f8df55fd6d05744458fc8acd798ee289
Tyler Yang <tjyang2005@gmail.com>
2026-05-06 16:15:30 -0500

    clean: remove all content

--- raw-files/ml_soccer/asdf.py
+++
@@ -1,6 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-page = requests.get('https://www.premierleague.com/results?co=1&se=489&cl=-1');
-soup = BeautifulSoup(page.content, 'html.parser')
-
